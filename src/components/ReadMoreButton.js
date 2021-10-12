5df6a61abd0d05e23bb2380d6ee69fec8497e072
Greg <greg_markarian@hotmail.com>
2021-10-12 17:51:50 +0300

    ADD: readmore button

---
+++ src/components/ReadMoreButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import cn from "classnames";
+
+function ReadMoreButton(props) {
+  return (
+    <div className={cn(props.className)}>
+      <button>{props.ReadMoreButtonName}</button>
+    </div>
+  );
+}
+
+export default ReadMoreButton;
