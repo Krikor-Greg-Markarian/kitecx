a4ffa85d5cfbb1fe60d3f619fea2e38900d60b67
Greg <greg_markarian@hotmail.com>
2021-10-12 17:52:04 +0300

    ADD: service item

---
+++ src/components/Servicesitem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import cn from "classnames";
+import Link from "next/link";
+
+function Servicesitem(props) {
+  return (
+    <div>
+      <div className={cn("building")}>
+        <div className={cn("transform translate-y-24")}>
+          <p className={cn("text-white text-4xl font-bold text-center")}>
+            {props.title}
+          </p>
+          <div className={cn("text-center pt-4")}>
+            <li className={cn("inline text-white font-bold")}>
+              <Link href="">
+                <a href="">{props.homeTag}</a>
+              </Link>
+            </li>
+            <li className={cn("inline text-white font-bold")}>
+              <Link href="">
+                <a href="">{props.servicesTag}</a>
+              </Link>
+            </li>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Servicesitem;
