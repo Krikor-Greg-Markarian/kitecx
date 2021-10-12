b9cb966496a60527cca344f528d95ebc0f5015bc
Greg <greg_markarian@hotmail.com>
2021-10-12 17:51:41 +0300

    ADD: new components

---
+++ src/components/ProfessionalItem.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { FaCouch, FaRegBuilding, FaBuilding } from "react-icons/fa";
+import ReadMoreButton from "./ReadMoreButton";
+import cn from "classnames";
+import styles from "../../styles/rollOver.module.css";
+
+function ProfessionalItem(props) {
+  const [isHovered, setisHovered] = useState(false);
+  return (
+    <div
+      className={cn("bg-white p-6 py-20 cursor-pointer", {
+        [styles.rollOver]: isHovered,
+      })}
+      onMouseEnter={() => setisHovered(true)}
+      onMouseLeave={() => setisHovered(false)}
+    >
+      <div className={cn("pb-3")}>
+        <img
+          className={cn("w-20 h-20 rounded-full")}
+          src={props.imageUrl}
+          alt=""
+        />
+      </div>
+      <p
+        className={cn("text-2xl", {
+          "text-white underline transition duration-500 ease-in-out transform ":
+            isHovered,
+        })}
+      >
+        {props.title}
+      </p>
+      <p className={cn("text-gray-500 pt-6 pb-6 w-9/12")}>
+        {props.description}
+      </p>
+      <ReadMoreButton
+        className={cn("font-bold text-sm", { "text-white": isHovered })}
+        ReadMoreButtonName={props.ReadMoreButtonName}
+      />
+    </div>
+  );
+}
+
+export default ProfessionalItem;
