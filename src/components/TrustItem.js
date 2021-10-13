1f032fb99543c0178606fb3cf79ae88ea0ceacd3
Greg <greg_markarian@hotmail.com>
2021-10-13 12:49:02 +0300

    FIX: redmorebutton section

--- src/components/TrustItem.js
+++ src/components/TrustItem.js
@@ -17,9 +17,9 @@ function TrustItem(props) {
           <div className={cn("flex justify-center items-center pt-10")}>
             <ReadMoreButton
               className={cn(
-                "bg-white w-2/12 text-center text-sm font-bold hover:bg-yellow-500 text-white pt-8"
+                "bg-white p-3 w-2/12 text-center float-right hover:bg-yellow-500  transition duration-500 ease-in-out"
               )}
-              ReadMoreButtonName={"LEARN MORE"}
+              ReadMoreButtonName={props.ReadMoreButtonName}
             />
           </div>
         </div>
