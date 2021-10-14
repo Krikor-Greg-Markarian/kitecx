16d38ff6815f58f933d6257086338960e3091367
Greg <greg_markarian@hotmail.com>
2021-10-14 12:53:43 +0300

    FIX: small screen h and w resizing

--- src/components/AboutItem.js
+++ src/components/AboutItem.js
@@ -10,17 +10,19 @@ function AboutItem(props) {
 
       <div class="grid grid-rows-1 md:grid-flow-col pt-12">
         <div className="row-span-1">
-          <img
-            className={cn("w-full")}
-            src="https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_233,h_244/https://www.gavias-theme.com/wp/kitecx/wp-content/uploads/2021/04/image-4.jpg"
-            alt=""
-          />
+          <div className={cn("sm:w-60 h-60")}>
+            <img
+              className={cn("w-full sm:w-60 h-60")}
+              src="https://sp-ao.shortpixel.ai/client/q_glossy,ret_img,w_233,h_244/https://www.gavias-theme.com/wp/kitecx/wp-content/uploads/2021/04/image-4.jpg"
+              alt=""
+            />
+          </div>
         </div>
         <div className={cn("pl-2")}>
-          <div className={cn("flex border-b-2 pb-3 pt-6")}>
+          <div className={cn("flex border-b-2 pb-3 pt-6 w-full")}>
             <div className={cn("flex-shrink")}>
               <img
-                className={cn("rounded-full w-20 h-20")}
+                className={cn("w-20 h-20")}
                 src={props.commpassImageUrl}
                 alt="compass"
               />
@@ -29,10 +31,10 @@ function AboutItem(props) {
               <p className={cn("text-gray-500 w-7/12")}>{props.title}</p>
             </div>
           </div>
-          <div className={cn("flex pb-2 pt-3")}>
-            <div className={cn("flex-shrink")}>
+          <div className={cn("flex pb-2 pt-3 w-full")}>
+            <div className={cn("flex-shrink ")}>
               <img
-                className={cn("rounded-full w-20 h-20")}
+                className={cn("w-20 h-20")}
                 src={props.housemapImageUrl}
                 alt="compass"
               />
