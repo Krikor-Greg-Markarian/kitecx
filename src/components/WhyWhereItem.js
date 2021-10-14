c00e81014d15d94d0033c24725a8fdbb1158a70f
Greg <greg_markarian@hotmail.com>
2021-10-14 16:09:59 +0300

    FIX: box size and text pt position

--- src/components/WhyWhereItem.js
+++ src/components/WhyWhereItem.js
@@ -7,12 +7,14 @@ function WhyWhereItem(props) {
       <div className={cn("flex-shrink")}>
         <div className={cn("bg-black")}>
           <div className={cn("transform translate-y-20 text-center")}>
-            <p className={cn("text-white h-60 w-60 text-2xl")}>{props.text}</p>
+            <p className={cn("text-white pt-4 h-64 w-72 text-2xl")}>
+              {props.text}
+            </p>
           </div>
         </div>
       </div>
       <div className={cn("flex-shrink")}>
-        <img className={cn("h-60 w-60")} src={props.imageUrl} alt="" />
+        <img className={cn("h-64 w-72")} src={props.imageUrl} alt="" />
       </div>
     </div>
   );
