4aa711751f7bec7dbf2f1b3e4ea48aa308ba0bb6
Greg <greg_markarian@hotmail.com>
2021-10-15 10:05:46 +0300

    FIX: bg- black p-14 instead of p-11

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -5,7 +5,7 @@ import ReadMoreButton from "./ReadMoreButton";
 function BusinessCard(props) {
   return (
     <div className={cn("pt-14")}>
-      <div className={cn("bg-black p-11 ")}>
+      <div className={cn("bg-black p-14")}>
         <div className={cn("pb-8 text-center")}>
           <sup className={cn("text-white inline font-normal text-3xl")}>
             {props.dolorSign}
@@ -40,7 +40,7 @@ function BusinessCard(props) {
         <div className={cn("pt-6 pb-2 text-center")}>
           <ReadMoreButton
             className={cn(
-              "bg-white p-3 w-8/12 hover:bg-yellow-500  transition duration-500 ease-in-out text-sm font-bold"
+              "bg-white p-3 w-12/12 hover:bg-yellow-500  transition duration-500 ease-in-out text-sm font-bold"
             )}
             ReadMoreButtonName={props.ReadMoreButtonName}
           />
