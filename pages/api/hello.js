17c43a4617bc7234cb064d25ca8ac2b4c1ce174d
Greg <greg_markarian@hotmail.com>
2021-10-18 13:50:15 +0300

    WIP

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,149 +4,149 @@ import { data } from "autoprefixer";
 
 export default function handler(req, res) {
   const data = {
-    about: [
+    "about": [
       {
-        smallTitle: "ABOUT COMPANY",
-        titleOne: "WE PROVIDE THE BEST ARCHITECTS",
-        description:
+        "smallTitle": "ABOUT COMPANY",
+        "titleOne": "WE PROVIDE THE BEST ARCHITECTS",
+        "description":
           "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, simply free text by injected humour, or randomised.",
-        commpassImageUrl:
+        "commpassImageUrl":
           "https://cdn3.iconfinder.com/data/icons/real-estate-line-12/100/33-house_sizes_ruler_House_Measurement_ArchitectConstruction-512.png",
-        title: "There are many variations passages of available.",
-        housemapImageUrl:
+        "title": "There are many variations passages of available.",
+        "housemapImageUrl":
           "https://cdn3.iconfinder.com/data/icons/real-estate-line-12/100/10-architecture_house_Blueprint_Construction_PlanHouse_Sizereal_estateProperty-512.png",
-        anotherTitle: "There are many variations passages of available.",
-      },
+        "anotherTitle": "There are many variations passages of available."
+      }
     ],
 
-    professionalItem: [
+    "professionalItem": [
       {
-        imageUrl:
+        "imageUrl":
           "https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287__480.jpg",
-        title: "Interior Design",
-        description:
+        "title": "Interior Design",
+        "description":
           "Lorem ipsum dolor sit amet, consectetur notted adipiscing elit sed do.",
-        ReadMoreButtonName: "READ MORE",
+        "ReadMoreButtonName": "READ MORE"
       },
       {
-        imageUrl:
+        "imageUrl":
           "https://cdn.pixabay.com/photo/2020/11/08/00/17/waterfall-5722620__340.jpg",
-        title: "Architecture",
-        description:
+        "title": "Architecture",
+        "description":
           "Lorem ipsum dolor sit amet, consectetur notted adipiscing elit sed do.",
-        ReadMoreButtonName: "READ MORE",
+        "ReadMoreButtonName": "READ MORE"
       },
       {
-        imageUrl:
+        "imageUrl":
           "https://cdn.pixabay.com/photo/2021/09/17/12/12/coffee-6632524__340.jpg",
-        title: "Exterior Design",
-        description:
+        "title": "Exterior Design",
+        "description":
           "Lorem ipsum dolor sit amet, consectetur notted adipiscing elit sed do.",
-        ReadMoreButtonName: "READ MORE",
+        "ReadMoreButtonName": "READ MORE"
       },
       {
-        imageUrl:
+        "imageUrl":
           "https://cdn.pixabay.com/photo/2020/07/30/10/26/asian-woman-5450041__340.jpg",
-        title: "Furniture & Decor",
-        description:
+        "title": "Furniture & Decor",
+        "description":
           "Lorem ipsum dolor sit amet, consectetur notted adipiscing elit sed do.",
-        ReadMoreButtonName: "READ MORE",
+        "ReadMoreButtonName": "READ MORE"
       },
       {
-        imageUrl:
+        "imageUrl":
           "https://cdn.pixabay.com/photo/2015/06/01/00/20/man-792821__340.jpg",
-        title: "Designing Solutions",
-        description:
+        "title": "Designing Solutions",
+        "description":
           "Lorem ipsum dolor sit amet, consectetur notted adipiscing elit sed do.",
-        ReadMoreButtonName: "READ MORE",
+        "ReadMoreButtonName": "READ MORE"
       },
       {
-        imageUrl:
+        "imageUrl":
           "https://cdn.pixabay.com/photo/2021/09/29/13/17/redwood-national-park-6667452__340.jpg",
-        title: "Custom Solution",
-        description:
+        "title": "Custom Solution",
+        "description":
           "Lorem ipsum dolor sit amet, consectetur notted adipiscing elit sed do.",
-        ReadMoreButtonName: "READ MORE",
-      },
+        "ReadMoreButtonName": "READ MORE"
+      }
     ],
-    whyWhereitem: [
+    "whyWhereitem": [
       {
-        text: "Why Should You Choose Kitecs?",
-        imageUrl:
-          "https://cdn.pixabay.com/photo/2017/03/22/17/39/kitchen-2165756__340.jpg",
+        "text": "Why Should You Choose Kitecs?",
+        "imageUrl":
+          "https://cdn.pixabay.com/photo/2017/03/22/17/39/kitchen-2165756__340.jpg"
       },
       {
-        text: "We’re Award Winnings Studio",
-        imageUrl:
-          "https://cdn.pixabay.com/photo/2015/04/20/13/30/kitchen-731351__340.jpg",
-      },
+        "text": "We’re Award Winnings Studio",
+        "imageUrl":
+          "https://cdn.pixabay.com/photo/2015/04/20/13/30/kitchen-731351__340.jpg"
+      }
     ],
-    projectInfoItem: [
+    "projectInfoItem": [
       {
-        imagUrl:
+        "imagUrl":
           "https://cdn.pixabay.com/photo/2021/08/23/04/02/woman-6566600__340.jpg",
-        number: "6,816",
-        text: "PROJECT SUCCESS",
+        "number": "6,816",
+        "text": "PROJECT SUCCESS"
       },
       {
-        imagUrl:
+        "imagUrl":
           "https://cdn.pixabay.com/photo/2020/11/08/00/17/waterfall-5722620__340.jpg",
-        number: "916",
-        text: "TEAM MEMBERS",
+        "number": "916",
+        "text": "TEAM MEMBERS"
       },
       {
-        imagUrl:
+        "imagUrl":
           "https://cdn.pixabay.com/photo/2021/10/03/04/21/woman-6676901__340.jpg",
-        number: "218",
-        text: "SERVICE PROVIDING",
+        "number": "218",
+        "text": "SERVICE PROVIDING"
       },
       {
-        imagUrl:
+        "imagUrl":
           "https://cdn.pixabay.com/photo/2020/11/24/11/24/sika-deer-5772242__340.jpg",
-        number: "9,660",
-        text: "HAPPY CUSTOMERS",
-      },
+        "number": "9,660",
+        "text": "HAPPY CUSTOMERS"
+      }
     ],
-    pricingPlan: [
+    "pricingPlan": [
       {
-        title: "Interior Sketch",
-        description:
-          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered.",
+        "title": "Interior Sketch",
+        "description":
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered."
       },
       {
-        title: "3D Modeling",
-        description:
-          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered.",
+        "title": "3D Modeling",
+        "description":
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered."
       },
       {
-        title: "Room Measurement",
-        description:
-          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered.",
-      },
+        "title": "Room Measurement",
+        "description":
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered."
+      }
     ],
 
-    businessCard: {
-      standardBusiness: {
-        dolorSign: "$",
-        price: "60",
-        month: "/MONTHLY",
-        title: "Standard Business",
-        visualization: "3D Visualization",
-        planning: "Planning Solution",
-        selection: "Selection of Materials",
-        construction: "10 Construction Drawings",
-        ReadMoreButtonName: "CHOOSE PLAN",
+    "businessCard": {
+      "standardBusiness": {
+        "dolorSign": "$",
+        "price": "60",
+        "month": "/MONTHLY",
+        "title": "Standard Business",
+        "visualization": "3D Visualization",
+        "planning": "Planning Solution",
+        "selection": "Selection of Materials",
+        "construction": "10 Construction Drawings",
+        "ReadMoreButtonName": "CHOOSE PLAN",
       },
-      largeBusiness: {
-        dolorSign: "$",
-        price: "89",
-        month: "/MONTHLY",
-        title: "Standard Business",
-        visualization: "3D Visualization",
-        planning: "Planning Solution",
-        selection: "Selection of Materials",
-        construction: "15 Construction Drawings",
-        ReadMoreButtonName: "CHOOSE PLAN",
+      "largeBusiness": {
+        "dolorSign": "$",
+        "price": "89",
+        "month": "/MONTHLY",
+        "title": "Standard Business",
+        "visualization": "3D Visualization",
+        'planning': "Planning Solution",
+        "selection": "Selection of Materials",
+        "construction": "15 Construction Drawings",
+        "ReadMoreButtonName": "CHOOSE PLAN",
       },
     },
   };
